4881adc16ad8013289c4ce10a3d2bccbb99089e7
aokisok <sevenclickok@gmail.com>
2019-10-20 15:39:24 +0100

    add softFailWith

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import { retryWithBackoff } from "./retry-with-backoff";
 import { batchWithProgress, BatchJob } from "./batch-with-progress";
 import { batch } from "./batch";
+import { softFailWith } from "./soft-fail-with";
 
-export { retryWithBackoff, batch, batchWithProgress, BatchJob }
+export { retryWithBackoff, softFailWith, batch, batchWithProgress, BatchJob }
+
